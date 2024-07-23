067d06cd4733058ae71c683bef8f4988bc2dead3
Obada-Badee <obadabadee3@hotmail.com>
2024-07-23 03:07:44 +0300

    Task 1. Simple pagination

---
+++ 0x00-pagination/1-simple_pagination.py
@@ -0,0 +1,40 @@
+#!/usr/bin/env python3
+""" Simple helper function Module """
+from typing import Tuple, List
+import csv
+import math
+
+
+def index_range(page: int, page_size: int) -> Tuple[int, int]:
+    """Return tuple of size two containing a start index and an end index"""
+    start_index = (page - 1) * page_size
+    end_index = start_index + page_size
+    return (start_index, end_index)
+
+
+class Server:
+    """Server class to paginate a database of popular baby names.
+    """
+    DATA_FILE = "Popular_Baby_Names.csv"
+
+    def __init__(self):
+        self.__dataset = None
+
+    def dataset(self) -> List[List]:
+        """Cached dataset
+        """
+        if self.__dataset is None:
+            with open(self.DATA_FILE) as f:
+                reader = csv.reader(f)
+                dataset = [row for row in reader]
+            self.__dataset = dataset[1:]
+
+        return self.__dataset
+
+    def get_page(self, page: int = 1, page_size: int = 10) -> List[List]:
+        """ takes two integer arguments """
+        assert type(page) is int and type(page_size) is int
+        assert page > 0 and page_size > 0
+        start, end = index_range(page, page_size)
+        data = self.dataset()
+        return data[start: end]
